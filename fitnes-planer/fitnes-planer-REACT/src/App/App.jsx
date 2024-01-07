b34d8c47d77a4ced40187f6215955635de216c12
Andrija Maric <andrijamaric01@gmail.com>
2024-01-07 16:05:59 +0100

    Dodavanje App stranice koja je navbar sa ruterom

---
+++ fitnes-planer/fitnes-planer-REACT/src/App/App.jsx
@@ -0,0 +1,226 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faBars } from '@fortawesome/free-solid-svg-icons';
+
+// Ensure the path for your CSS file is correct
+import './App.css';
+
+// Import your components
+import Home from "../Home/Home.jsx";
+
+
+
+
+function App() {
+
+
+    // Promenljiva i f-ja za Menu dugme na telefonu
+    const [menuOpen, setMenuOpen] = useState(false);  // da li je kliknuto menu dugme
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); // Toggle the state
+        console.log('Menu toggled. New state:', !menuOpen);
+    };
+    // Mobilni podmeni 1
+    const [subMenu1Open, setSubMenu1Open] = useState(false);  // da li je kliknuto 1 dugme
+    const [subMenu2Open, setSubMenu2Open] = useState(false);
+    const [subMenu3Open, setSubMenu3Open] = useState(false);
+    const [subMenu4Open, setSubMenu4Open] = useState(false);
+    const toggleSubMenu1 = () => {
+        resetSubMenu();
+        setSubMenu1Open(!subMenu1Open); // Toggle the state
+        console.log('Submenu 1 toggled. New state:', !subMenu1Open); 
+    };
+    const toggleSubMenu2 = () => {
+        resetSubMenu();
+        setSubMenu2Open(!subMenu2Open); 
+        console.log('Submenu 2 toggled. New state:', !subMenu2Open); 
+    };
+    const toggleSubMenu3 = () => {
+        resetSubMenu();
+        setSubMenu3Open(!subMenu3Open); 
+        console.log('Submenu 3 toggled. New state:', !subMenu3Open); 
+    };
+    const toggleSubMenu4 = () => {
+        resetSubMenu();
+        setSubMenu4Open(!subMenu4Open); 
+        console.log('Submenu 4 toggled. New state:', !subMenu4Open); 
+    };
+
+    const resetSubMenu = () => {
+        setSubMenu1Open(false);
+        setSubMenu2Open(false);
+        setSubMenu3Open(false);
+        setSubMenu4Open(false);
+        console.log('Svi sub-meniji su skupljeni!'); 
+    }
+
+    // ZA NESTAJACE PROCITAM DUGME U MOBILNOM NAVBARU
+    const handleButtonClick = () => {
+        window.location.href = '/';
+    };
+
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+  
+
+
+
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const header = document.querySelector('.header');
+            const navbar = document.querySelector('.navbar');
+            const headerBottom = header.getBoundingClientRect().bottom;
+
+            if (window.scrollY > headerBottom) {
+                navbar.classList.add('sticky-navbar');
+            } else {
+                navbar.classList.remove('sticky-navbar');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+
+    window.addEventListener('scroll', () => {
+        const logo = document.querySelector('.navbar-logo');
+        if (window.scrollY > 100) {
+            logo.classList.add('visible');
+        } else {
+            logo.classList.remove('visible');
+        }
+    });
+
+
+    return (
+        <Router>
+            <div className="app">
+                <header className="header">
+                    <div to="/" className="logo-container">
+                      <Link to="/" className="logo">
+                        <img src={Logo} alt="React logo"/>
+                      </Link>
+                      <Link to="/" className="logo">
+                        <h1 className="textLogo">FIT-PlAN</h1>
+                      </Link>
+                      
+                      </div>
+                <div className="login-button-wrap">
+                <Link to="/" className="login-button" >Kontakt</Link>
+                </div>
+                </header>
+
+                <nav className="navbar">
+
+                    {/* MENU DUGME ZA TELEFON */}
+                    <div className={`menu-icon ${menuOpen ? 'menu-icon-open' : 'menu-icon-closed'}`} onClick={() => { toggleMenu(); resetSubMenu(); }}>
+                        
+                        {menuOpen 
+                            ? <FontAwesomeIcon className="menu-icon-1" icon={faChevronLeft} />
+                            : <FontAwesomeIcon className="menu-icon-2" icon={faBars} />
+                        }
+
+                        <button 
+                            onMouseEnter={handleMouseEnter} 
+                            onMouseLeave={handleMouseLeave} 
+                            onClick={menuOpen ? () => { toggleMenu(); resetSubMenu(); } : handleButtonClick}
+                            className="navbar-logo"
+                        >
+                            <h1>FIT-PLAN</h1>
+                        </button>
+                    </div>
+
+                     {/* Panel that slides in and out */}
+                     <div className={`side-panel ${menuOpen ? 'open' : 'closed'}`}>
+                        <button className={`panel-btn-1 ${subMenu1Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu1Open(true)} onMouseLeave={() => setSubMenu1Open(false)}>1</button>
+                        <button className={`panel-btn-2 ${subMenu2Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu2Open(true)} onMouseLeave={() => setSubMenu2Open(false)}>2</button>
+                        <button className={`panel-btn-3 ${subMenu3Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu3Open(true)} onMouseLeave={() => setSubMenu3Open(false)}>3</button>
+                        <button className={`panel-btn-4 ${subMenu4Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu4Open(true)} onMouseLeave={() => setSubMenu4Open(false)}>4</button>
+                    </div>
+
+
+                    <div className={`side-panel-1 ${subMenu1Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu1Open(true)} onMouseLeave={() => setSubMenu1Open(false)}>
+                        <Link to="/" className="panel-link">Ep o Gilgamešu</Link>
+
+                    </div>
+
+                    <div className={`side-panel-2 ${subMenu2Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu2Open(true)} onMouseLeave={() => setSubMenu2Open(false)}>
+                        <Link to="/" className="panel-link">Bajka_o_ribaru_i_ribici</Link>
+                        
+                    </div>
+
+                    <div className={`side-panel-3 ${subMenu3Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu3Open(true)} onMouseLeave={() => setSubMenu3Open(false)}>
+                        <Link to="/">Tuga</Link>
+
+                    </div>
+
+                    <div className={`side-panel-4 ${subMenu4Open ? 'open' : 'closed'}`} onMouseEnter={() => setSubMenu4Open(true)} onMouseLeave={() => setSubMenu4Open(false)}>
+                        <Link to="/">Mit o Sizifu</Link>
+
+                    </div>
+
+
+
+
+
+
+
+                    {/*REGULAR NAVBAR*/}
+                    <div className="dropdown one">
+                        <button className="dropbtn">GRUPE</button>
+                        <div className="dropdown-content">
+                            <Link to="/">Ep o Gilgamešu✅</Link>
+
+                        </div>
+                    </div>
+                    <div className="dropdown two">
+                        <button className="dropbtn">VEZBE</button>
+                        <div className="dropdown-content">
+                            <Link to="/">Ep o Gilgamešu✅</Link>
+
+                        </div>
+                    </div>
+                    <div className="dropdown three">
+                        <button className="dropbtn">TRENINZI</button>
+                        <div className="dropdown-content">
+                            <Link to="/">Ep o Gilgamešu✅</Link>
+
+                        </div>
+                    </div>
+                    
+                </nav>
+
+                <main className="portal">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+
+
+                        
+
+                        
+                    </Routes>
+                </main>
+
+                <footer className="footer">
+                    <p>© 2023 FIT-PLAN. Sva prava su zadržana.</p>
+                </footer>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
+
---
+++ fitnes-planer/fitnes-planer-REACT/src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+
+function Home() {
+
+    return (
+            <div className="app">
+                <h1> Home </h1>
+            </div>
+
+    );
+}
+
+export default Home;
+
