eb8d4e1a333adf4da2d2620caad064b1096360cb
Andrija Maric <andrijamaric01@gmail.com>
2024-01-09 02:32:12 +0100

    Home page elementi
    Dodao sam elemente na home page i doradio funkcionalnost navbara iz App.js
    Dodao sam header u home koji je namenjen za video bez zvuka
    Dodao sam elemente (kruzice) sa opisom usluga
    Dodao sam "Sta korisnici kazu" u home
    Obrisao sam nepotrebne elemente pojednostavio kod i sredio CSS

--- fitnes-planer/fitnes-planer-REACT/src/App/App.jsx
+++ fitnes-planer/fitnes-planer-REACT/src/App/App.jsx
@@ -1,82 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Logo from '../images/logo.png';
-
-
-// Ensure the path for your CSS file is correct
 import './App.css';
-
-// Import your components
 import Home from "../Home/Home.jsx";
 
-
-
-
 function App() {
-
-  const [isSticky, setSticky] = useState(false);
-
-  const handleScroll = () => {
-    const isTop = window.scrollY < 100;
-    setSticky(!isTop);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
-
-
-    return (
-        <Router>
-            <div className="app">
-                
-                <nav className={`navbar ${isSticky ? 'sticky' : ''}`}>
-                    <div to="/" className="logo-container">
-                      <Link to="/" className={`logo ${isSticky ? 'fade-out' : ''}`}>
-                        <img src={Logo} alt="React logo"/>
-                      </Link>
-                       <Link to="/" className={`textLogo ${isSticky ? 'slide-in' : ''}`}>
-                        <h1 className="textLogo">FIT PLAN</h1>
-                      </Link>
-                    </div>
-
-
-
-                     <div className={`buttons ${isSticky ? 'center' : ''}`}>
-                      <div className="dropdown one">
-                          <Link to="/">Grupe {/* Usluge*/}</Link>
-                      </div>
-
-                      <div className="dropdown two ">
-                          <Link to="/">Vezbe {/* Galerija*/}</Link>
-                          
-                      </div>
-                      <div className="dropdown three">
-                           <Link to="/">Treninzi {/* Kontakt*/}</Link>
-                      </div>
-                    </div>
-
-                </nav>
-
-                <main className="portal">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                </main>
-
-                <footer className="footer">
-                    <p>© 2023 FIT-PLAN. Sva prava su zadržana.</p>
-                </footer>
+  return (
+    <Router>
+      <div className="app">
+        <nav className="navbar">
+          <div className="navbar-content">
+            <Link to="/" className="logo">
+              <img src={Logo} alt="React logo"/>
+              <h1 className="textLogo">FIT PLAN</h1>
+            </Link>
+            <div className="nav-links">
+              <Link to="/">Grupe</Link>
+              <Link to="/">Vezbe</Link>
+              <Link to="/">Treninzi</Link>
             </div>
-        </Router>
-    );
+          </div>
+        </nav>
+
+        <main className="portal">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          <p>© 2023 FIT-PLAN. Sva prava su zadržana.</p>
+        </footer>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
--- fitnes-planer/fitnes-planer-REACT/src/Home/Home.jsx
+++ fitnes-planer/fitnes-planer-REACT/src/Home/Home.jsx
@@ -2,12 +2,92 @@ import React, { useEffect, useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import './Home.css';
 
+
+
+// videi
+import video1 from './images/video1.mov';
+
+// Importing images
+import client1 from './images/home1.png';
+
+// Importovanje krugova
+import krug1 from './images/krug1.png';
+import krug2 from './images/krug2.png';
+import krug3 from './images/krug3.png';
+import krug4 from './images/krug4.png';
+import krug5 from './images/krug5.png';
+
+
+
+
 function Home() {
   const [loaded, setLoaded] = useState(false);
+  const [activeTestimonial, setActiveTestimonial] = useState(0);
 
   useEffect(() => {
     setLoaded(true);
   }, []);
+
+
+useEffect(() => {
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const newHeight = Math.max(100, 350 - scrollPosition); // Adjusted for min height
+    const scaleFactor = newHeight / 350; // Calculate scale factor
+
+    const header = document.querySelector('.header');
+    const headerText = document.querySelector('.header-text');
+    const services = document.querySelector('.services');
+
+    header.style.height = `${newHeight}px`;
+    headerText.style.transform = `scale(${scaleFactor})`;
+
+    // Define max and min padding values
+    const maxPadding = 120;
+    const minPadding = 0;
+
+    // Calculate dynamic padding based on scroll position
+    let dynamicPadding = minPadding + scrollPosition;
+    dynamicPadding = Math.min(dynamicPadding, maxPadding); // Ensures padding isn't more than max
+
+    // Set dynamic padding-top for .services
+    services.style.paddingTop = `${dynamicPadding}px`;
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
+
+
+  const testimonials = [
+    {
+      quote: "The attention to detail and care in their service is unparalleled! My body has never looked better.",
+      client: "Predrag ********",
+      image: client1 // Reference the imported image here
+    },
+    {
+      quote: "The attention to detail and care in their service is unparalleled! My body has never looked better.",
+      client: "Petar ********",
+      image: client1 // Reference the imported image here
+    },
+    {
+      quote: "The attention to detail and care in their service is unparalleled! My body has never looked better.",
+      client: "Filip ********",
+      image: client1 // Reference the imported image here
+    },
+    // ... other testimonials
+  ];
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveTestimonial((prevActiveTestimonial) => (prevActiveTestimonial + 1) % testimonials.length);
+    }, 5000); // Change testimonials every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup the interval on component unmount
+  }, [testimonials.length]);
+
 
   const fadeIn = useSpring({
     opacity: loaded ? 1 : 0,
@@ -15,61 +95,104 @@ function Home() {
     config: { duration: 1000 },
   });
 
+
+  const nextTestimonial = () => {
+    setActiveTestimonial((prevActiveTestimonial) => (prevActiveTestimonial + 1) % testimonials.length);
+  };
+
+
   return (
     <animated.div style={fadeIn} className="home-container">
+
+
       <header className="header">
-        <h1 className="company-name">Luxury Cleaners</h1>
-        <p className="slogan">Elevate Your Space to Pure Luxury</p>
+      <div className="header-content">
+        <video autoPlay loop muted className="header-video" style={{ position: 'absolute', width: '100%', left: '50%', top: '50%', height: 'auto', transform: 'translate(-50%, -50%)', zIndex: '-1'}}>
+          <source src={video1} type="video/mp4" />
+        </video>
+        <div className="header-text">
+            <h1 className="company-name">(064-50-60-200)</h1>
+            <p className="slogan">Pozovite odmah</p>
+        </div>
+      </div>
       </header>
 
+      <div className="sekcije">
+
       <section className="services">
+        {/* Replace with actual service details */}
         <div className="service-card">
           <div className="service-icon">
-            <img src="/car-interior-icon.png" alt="Car Interior Cleaning" />
+            <img src={krug1} alt="Krug1" />
           </div>
-          <h2 className="service-title">Car Interior Cleaning</h2>
+          <h2 className="service-title">Treninzi</h2>
           <p className="service-description">
-            Experience the ultimate in car interior cleanliness and luxury.
+            Nasi treninzi sastoje se od pojedinacnih vezbi za svaku grupu misica.
           </p>
         </div>
 
         <div className="service-card">
           <div className="service-icon">
-            <img src="/sofa-icon.png" alt="Sofa Cleaning" />
+            <img src={krug2} alt="Krug2" />
           </div>
-          <h2 className="service-title">Sofa Cleaning</h2>
+          <h2 className="service-title">Vezbe</h2>
           <p className="service-description">
-            Transform your sofa into a masterpiece of comfort and style.
+            Transformisite svoje telo jednostavnim planom vezbi.
           </p>
         </div>
 
         <div className="service-card">
           <div className="service-icon">
-            <img src="/awning-icon.png" alt="Awning Cleaning" />
+            <img src={krug3} alt="Krug3" />
           </div>
-          <h2 className="service-title">Awning Cleaning</h2>
+          <h2 className="service-title">Proteini</h2>
           <p className="service-description">
-            Renew and protect your outdoor awnings with our premium service.
+            Zastite i obnovite misice nasim proteinskim sejkovima.
           </p>
         </div>
 
         <div className="service-card">
           <div className="service-icon">
-            <img src="/carpet-icon.png" alt="Carpet Cleaning" />
+            <img src={krug4} alt="Krug4" />
           </div>
-          <h2 className="service-title">Carpet Cleaning</h2>
+          <h2 className="service-title">Teretane</h2>
           <p className="service-description">
-            Elevate your living spaces with our luxurious carpet cleaning.
+            Poboljsajte celokupan dozivljaj posetom nase teretane.
+          </p>
+        </div>
+
+        <div className="service-card">
+          <div className="service-icon">
+            <img src={krug5} alt="Krug4" />
+          </div>
+          <h2 className="service-title">Ishrana</h2>
+          <p className="service-description">
+            Poboljsate svoje zdravlje nasim neverovatnim programom ishrane!
           </p>
         </div>
       </section>
 
-      <footer className="footer">
-        <p className="contact-info">
-          Contact us at: <a href="tel:+123456789">+1 (123) 456-789</a>
-        </p>
-        <p className="address">123 Luxury Lane, City, State, Zip</p>
-      </footer>
+
+
+    <section className="testimonials">
+        <h2>Šta nase mušterije kažu</h2>
+        <div className="testimonial-container">
+          {testimonials.map((testimonial, index) => (
+            <animated.div key={index} className={`testimonial ${activeTestimonial === index ? 'active' : ''}`}>
+              <img src={testimonial.image} alt="Client" className="client-photo" />
+              <blockquote>
+                "{testimonial.quote}"
+              </blockquote>
+              <cite>- {testimonial.client}</cite>
+            </animated.div>
+          ))}
+        </div>
+        <button onClick={nextTestimonial}>Next</button>
+    </section>
+
+
+    </div>
+
     </animated.div>
   );
 }
